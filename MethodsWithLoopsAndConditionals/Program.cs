ecc82662683f4a7d4be097d485cc3b9a9834daa1
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-12 11:19:53 -0500

    exercise one

---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//exercise one- write this loop 15 times
+//"for" loops execute a specific number of times
+
+for (int i = 0; i < 5; i++)//for keyword (initializer;conditional;incrementer)
+{
+    Console.WriteLine($"{i}");//body
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
